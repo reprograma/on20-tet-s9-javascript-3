3d3056773c8edbd865ccd2e90ac4fa003bd5a81c
Pati Rocha <patirocha.dev@gmail.com>
2022-10-12 19:36:11 -0300

    Pasta renomeada

---
+++ exercicios/pati-rocha/projeto-casa/index.js
@@ -0,0 +1,31 @@
+
+/*-----Exemplo de aplicação do método .reduce()-----*/
+
+/*const carrinhoCompras = [
+    {
+      nome: "Café",
+      preco: 9.90
+    },
+    {
+      nome: "Arroz",
+      preco: 10.95
+    },
+    {
+      nome: "Açucar",
+      preco: 5.59
+    }
+  ];
+  
+ const total =  carrinhoCompras.reduce((acc, item) => {
+    return acc + item.preco;
+  },0);
+
+  console.log(total);*/
+
+ const data = new Date();
+ const dataFormatada = data.toLocaleDateString()
+ console.log(dataFormatada);
+
+
+
+
