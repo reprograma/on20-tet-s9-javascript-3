ad9052c2a2521c13395fbb5a13c0541ae64baa4e
Rokssane Marina <rokssane@gmail.com>
2022-10-13 20:13:44 -0300

    foi só ladeira a baixo

---
+++ exercicios/nome-aluna/casa-rocks/index.html/casa/index.js
@@ -0,0 +1,35 @@
+let series = [{
+    nome: "Friends",
+    ano: "1994 - 2004",
+    assistir: "HBO Max",
+    temporadas: "10 temporadas"
+},
+{
+    nome: "Chuck",
+    ano: "2007 - 2012",
+    assistir: "Amazon Prime Video",
+    temporadas: "5 temporadas"
+},
+{
+    nome: "Gilmore Girls",
+    ano: "2000 - 2007",
+    assistir: "Netflix",
+    temporadas: "7 temporadas"
+},
+{
+    nome: "Modern Family ",
+    ano: "2009 - 2020 ",
+    assistir: "Star Plus",
+    temporadas: "11 temporadas"
+},
+{
+    nome: "Bridgerton",
+    ano: "2020 - em andamento",
+    assistir: "Netflix",
+    temporadas: "2 temporadas"
+}]
+
+// // let nomeDaSerie = document.querySelector()
+// let anoDuracao = 
+// let ondeAssistir = 
+// let qtoTemporadas = 
