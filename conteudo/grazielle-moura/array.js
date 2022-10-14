de9acc0aeff6994e3022f6cd612cc7cb633f56c5
Grazielle Moura <mouragrazielle@gmail.com>
2022-10-14 13:37:46 -0300

    Fazendo site com Cards usando Json

---
+++ conteudo/grazielle-moura/array.js
@@ -0,0 +1,13 @@
+const alunas = ['Bea', 'Ju', 'Gabi']
+// console.log(alunas) 
+
+const turmas = new Array('on18', 'on20')
+// console.log(turmas)
+
+const tecnologias = Array.of('CSS', 'HTML', 'JavaScript')
+// console.log(tecnologias)
+
+const alfabeto = 'lilit bandeira costa'.split(' ') 
+// console.log(alfabeto)
+
+console.log(alfabeto[2])
