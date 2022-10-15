f130ea7825b2d0a83cd90ead1761b2dcaaac2871
Gisele Fragoso <gisele-fragoso@outlook.com>
2022-10-14 22:43:26 -0300

    terminei a atividade mas com a ajuda da resoluçao do exercicio

---
+++ conteudo/index.js
@@ -0,0 +1,7 @@
+const horario = prompt('digite um horário');
+
+if(horario <120){
+alert('é dia')
+} else{
+    alert ('é noite')
+}
--- exercicios/gisele/para-sala/pratica/script.js
+++ exercicios/gisele/para-sala/pratica/script.js
@@ -8,3 +8,16 @@ const objetoJson = {
 }
 
 // Suponha que nosso projeto consiste em um site de informações sobre cantoras brasileiras e na página em questão a usuária selecionou a cantora Liniker, e a API nos retornou um JSON com todas as informações que precisamos (armazenado na variável objetoJson acima), então devemos popular nosso HTML com essas informações utilizando JavaScript 
+const photography= document.getElementById('photography')
+console.log(foto)
+const title = document.querySelector('#title')
+const gender = document.getElementById('#gender')
+const discpgraphy = document.getElementById('discography')
+const instagram = document.getElementById('instagram')
+
+preencherDads = () =>{
+  photography.setAttribute('src', objetoJson.foto)
+  title.innerText = objetoJson.nome;
+  gender.innerHTML = objetoJson.generos;
+}
+ 
